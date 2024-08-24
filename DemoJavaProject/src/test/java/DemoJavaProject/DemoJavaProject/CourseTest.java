f186f5187ff0a1fff316e4969e40a400ebeba123
Maqsud Kukaswadia <Maqsud Kukaswadia@DESKTOP-AOEP0U2>
2024-08-24 13:00:15 +0530

    I have Successfully uploaded project.

---
+++ DemoJavaProject/src/test/java/DemoJavaProject/DemoJavaProject/CourseTest.java
@@ -0,0 +1,17 @@
+package DemoJavaProject.DemoJavaProject;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class CourseTest 
+{
+   public static void main(String[] args) {
+	
+	   System.out.println("Selenium Java Accomplished!");
+	   System.out.println("API Testing with Rest Assured Accomplished!");
+	   System.out.println("Git and Github is Successfully Executed!");
+	   
+}
+   
+}
