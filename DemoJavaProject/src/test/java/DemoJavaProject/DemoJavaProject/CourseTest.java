56060a8d11bbec79e8b1791443fb69d6a0f87d4d
Maqsud Kukaswadia <Maqsud Kukaswadia@DESKTOP-AOEP0U2>
2024-08-24 13:11:20 +0530

    Session on Git is completed!

--- DemoJavaProject/src/test/java/DemoJavaProject/DemoJavaProject/CourseTest.java
+++ DemoJavaProject/src/test/java/DemoJavaProject/DemoJavaProject/CourseTest.java
@@ -11,6 +11,7 @@ public class CourseTest
 	   System.out.println("Selenium Java Accomplished!");
 	   System.out.println("API Testing with Rest Assured Accomplished!");
 	   System.out.println("Git and Github is Successfully Executed!");
+	   System.out.println("Training phase is completed!");
 	   
 }
    
